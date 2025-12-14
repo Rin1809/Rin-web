90ab7d6fcfc5bf0e739aa6babbc83ba107a5aefc
Rin1809 <Rinn1913@gmail.com>
2025-12-14 21:15:45 +0700

    cursor trail effect

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import Header from './components/Header/Header';
 import Hero from './components/Hero/Hero';
 import ParticleBackground from './components/Background/ParticleBackground';
+import CursorTrail from './components/Cursor/CursorTrail';
 
 function App() {
   return (
     <div className="app">
+      <CursorTrail />
       <ParticleBackground />
       <Header />
       <Hero />
